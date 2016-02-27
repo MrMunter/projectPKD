5dba03e96ca94ccc142e78a9c4a14dddb963e7a0
MrMunter <victormunthe@gmail.com>
2016-02-27 15:40:18 +0100

    Update game.hs

--- game.hs
+++ game.hs
@@ -4,14 +4,14 @@ import Data.Array
 
 data Direction = Horisontal | Vertical 
 data Ship = Ship5 (Cord,Cord) | Ship4 (Cord,Cord) | Ship3 (Cord,Cord)  | Ship2 (Cord,Cord)
-data Square = Empty | Alive | Dead deriving (Eq, Show)
---data Names = Name1 | Name2
+data Square = Empty | Alive | Dead | Miss deriving (Eq, Show)
+
 type Board = [(Cord, Square)]
 type Cord = (Int,Int)
 type BoardSize = (Int, Int)
---type Name1 = String
---type Name2 = String
 
+
+boardSize = (10,10)
 
 
 
@@ -29,18 +29,28 @@ play = do
 		name2 <- inputName2
 		board1 <- boardSize1
 		board2 <- boardSize2
-		print board1
-		--go board1 board2
-		putStrLn ""
+		game board1 board2 name1 name2
+			where 
+				game b1 b2 n1 n2 = do 
+						putStrLn (n1 ++ " Attack")
+						cord <- getLine
+						cpbord (attack b2 (read cord)) 
+						if victory (attack b2 (read cord))
+							then do putStrLn (n1 ++ " win\n") >> main						 
+						else do
+						putStrLn (n2 ++ " Attack") 
+						cord2 <- getLine
+						cpbord (attack b1 (read cord2)) 
+						if victory (attack b1 (read cord2)) 
+							then do putStrLn (n2 ++ " Win\n") >> main
+						else game (attack b1 (read cord2)) (attack b2 (read cord)) n1 n2 
 
 
-{-placeShip2 :: IO Board
-placeShip2 = do
-				putStrLn "first cord?"
-				firstCord <- getLine 
-				putStrLn "second cord"
-				secondCord <- getLine
-				return (placeShip (Ship2 (read firstCord) (read secondCord)) (makeBoard (3,3)))-}
+ 
+attackIO :: Board -> IO Board 
+attackIO board = return board 
+
+
 
 
 inputName1 :: IO String
@@ -58,8 +68,6 @@ inputName2 = do
 
 boardSize1 :: IO Board
 boardSize1 = do 
-				putStrLn "How big do you want your board?\tWrite in the format: (x,y)"
-				cord <- getLine
 				putStrLn "Place your ship, 2 tiles. Format ((x1,y1),(x2,y2))"
 				cords <- getLine
 				putStrLn "Place your ship, 3 tiles. Format ((x1,y1),(x2,y2))"
@@ -68,12 +76,10 @@ boardSize1 = do
 				cords3 <- getLine
 				putStrLn "Place your ship, 5 tiles. Format ((x1,y1),(x2,y2))"
 				cords4 <- getLine
-				return (placeShip (Ship5 (read cords4)) (placeShip (Ship4 (read cords3)) ((placeShip (Ship3 (read cords2)) (placeShip (Ship2 (read cords)) (makeBoard (read cord)))))))
+				return (placeShip (Ship5 (read cords4)) (placeShip (Ship4 (read cords3)) ((placeShip (Ship3 (read cords2)) (placeShip (Ship2 (read cords)) (makeBoard (10,10)))))))
 
 boardSize2 :: IO Board
 boardSize2 = do 
-				putStrLn "How big do you want your board?\tWrite in the format: (x,y)"
-				cord <- getLine
 				putStrLn "Place your ship, 2 tiles. Format ((x1,y1),(x2,y2))"
 				cords <- getLine
 				putStrLn "Place your ship, 3 tiles. Format ((x1,y1),(x2,y2))"
@@ -82,30 +88,11 @@ boardSize2 = do
 				cords3 <- getLine
 				putStrLn "Place your ship, 5 tiles. Format ((x1,y1),(x2,y2))"
 				cords4 <- getLine
-				return (placeShip (Ship5 (read cords4)) (placeShip (Ship4 (read cords3)) ((placeShip (Ship3 (read cords2)) (placeShip (Ship2 (read cords)) (makeBoard (read cord)))))))
-				{-putStrLn "Place your ship, 3 tiles. Format ((x1,y1),(x2,y2))"
-					cords2 <- getLine
-				newBoard2 <- (placeShip (Ship3 (read cords2)) newBoard)-}				
+				return (placeShip (Ship5 (read cords4)) (placeShip (Ship4 (read cords3)) ((placeShip (Ship3 (read cords2)) (placeShip (Ship2 (read cords)) (makeBoard (10,10)))))))
 
-{-go b1 b2 = do
-						putStrLn "Player 1 attack"
-					  	attackIO <- getLine
-					  	newboard1 <- attack b2 (read attackIO)
-					  	if victory newboard1 then do
-					    	putStrLn "Player won!"
-					    	putStrLn ""
-					    	main
-					   	  else do
-					   		putStrLn "Player 2 attack"
-					   		attackIO2 <- getLine
-					    	newboard2 <- attack b1 (read attackIO2)  
-					    	if victory newboard2 then do
-					      		putStrLn "Computer won!"
-					      		putStrLn ""
-					      		main
-					           else
-					      		go newboard1 newboard2 -}
-							
+
+	
+				
 
 menu :: IO ()
 menu = do
@@ -129,16 +116,12 @@ rules = do
 victory :: Board -> Bool
 victory b = if (length (filter (== Alive) (sortAlive b))) == 0 then True else False
 
--- Removes a element from the board
-{-findCord :: Board -> Cord -> Board ->Board
-findCord (x:xs) (a,b) aux 
-	| (fst x) == (a,b) = xs
-	| otherwise = (x:aux) ++ (findCord xs (a,b) aux)  -}
+
 
 attack :: Board -> Cord -> Board
 attack (x:xs) (a,b)
 	| getCord (a,b) (x:xs) == False = (x:xs)
-	| (a,b) == (fst x) && (snd x) == Empty = (x:xs)
+	| (a,b) == (fst x) && (snd x) == Empty = ((a,b), Miss) : xs
 	| (a,b) == (fst x) && (snd x) == Alive = ((a,b), Dead) : xs
 	| otherwise = (x : attack xs (a,b))
 
@@ -216,3 +199,22 @@ validShip (Ship2 (x,c))  b = if (getCord x b) == True && (getCord c b) == True t
 validShip (Ship3 (x,c))  b = if (getCord x b) == True && (getCord c b) == True then True else False 
 validShip (Ship4 (x,c))  b = if (getCord x b) == True && (getCord c b) == True then True else False 
 validShip (Ship5 (x,c))  b = if (getCord x b) == True && (getCord c b) == True then True else False 
+
+
+
+cpbord :: Board -> IO()
+cpbord boardsize  = putStr" _  _  _  _  _  _  _  _  _  _\n" >> (printBattleground boardsize 1 10)
+
+	where
+		printBattleground :: Board -> Int -> Int -> IO()
+		printBattleground (a:xs) x y
+			|x == 10 && y == 1 = printShip (snd a) >> putStr "\n"
+			|x == 10 = printShip (snd a) >> putStr "\n" >>printBattleground (xs) 1 (y-1)
+			|x /= 10 = printShip (snd a) >> printBattleground (xs) (x+1) y
+
+
+		printShip :: Square -> IO()
+		printShip square 
+			|square == Dead = putStr "|x|"
+			|square == Miss = putStr "|#|"
+			|otherwise = putStr "|_|"
